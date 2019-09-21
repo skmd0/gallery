713ab6b7b533d0316d1a9c334853ae49dcb21da4
Domen Skamlic <skamlic@posteo.net>
2019-09-21 09:49:09 +0200

    Refactored form parsing logic into helpers.go function

--- controllers/users.go
+++ controllers/users.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"gallery/views"
 	"net/http"
-
-	"github.com/gorilla/schema"
 )
 
 // NewUsers is used to create anew Users controller
@@ -43,12 +41,8 @@ type SignupForm struct {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
