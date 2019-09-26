0c3128113cae5583b28259e1c496a6b1c7673021
Domen Skamlic <skamlic@posteo.net>
2019-09-26 20:27:48 +0200

    Added Create method for user db

--- models/users.go
+++ models/users.go
@@ -41,6 +41,11 @@ func (us *UserService) ByID(id uint) (*User, error) {
 	}
 }
 
+// Create will create the provided user in db
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
 // Close closes the UserService database connection
 func (us *UserService) Close() error {
 	return us.db.Close()
