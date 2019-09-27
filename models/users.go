e6b23970c504f1a52d939b8874344d716a8ef5c7
Domen Skamlic <skamlic@posteo.net>
2019-09-27 14:06:53 +0200

    Added AutoMigrate method for UserService

--- models/users.go
+++ models/users.go
@@ -81,9 +81,19 @@ func (us *UserService) Close() error {
 }
 
 // DestructiveReset drops the user table and rebuilds it
-func (us *UserService) DestructiveReset() {
-	us.db.DropTableIfExists(&User{})
-	us.db.AutoMigrate(&User{})
+func (us *UserService) DestructiveReset() error {
+	if err := us.db.DropTableIfExists(&User{}).Error; err != nil {
+		return err
+	}
+	return us.AutoMigrate()
+}
+
+// AutoMigrate tries to automigrate db
+func (us *UserService) AutoMigrate() error {
+	if err := us.db.AutoMigrate(&User{}).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 // User represents the users table in DB
