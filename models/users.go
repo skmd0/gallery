1a17c8eb49029abec795973b2c9706be5fe9d484
Domen Skamlic <skamlic@posteo.net>
2019-10-08 13:49:22 +0200

    Implemented the setRememberIfUnset to set default remmeber token if user is created without one

--- models/users.go
+++ models/users.go
@@ -114,19 +114,10 @@ func (uv *userValidator) ByRemember(token string) (*User, error) {
 
 // Create will create the provided user in db
 func (uv *userValidator) Create(user *User) error {
-	if user.Remember == "" {
-		token, err := rand.RememberToken()
-		if err != nil {
-			return err
-		}
-		user.Remember = token
-	}
-
-	err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacRemember)
+	err := runUserValFuncs(user, uv.bcryptPassword, uv.setRememberIfUnset, uv.hmacRemember)
 	if err != nil {
 		return err
 	}
-
 	return uv.UserDB.Create(user)
 }
 
@@ -170,6 +161,18 @@ func (uv *userValidator) hmacRemember(user *User) error {
 		return nil
 	}
 	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
+func (uv *userValidator) setRememberIfUnset(user *User) error {
+	if user.Remember != "" {
+		return nil
+	}
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+	user.Remember = token
 	return nil
 }
 
