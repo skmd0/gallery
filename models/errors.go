a855e35999c6abaff7d158238e38d4e66b63a8a4
Domen Skamlic <skamlic@posteo.net>
2019-10-10 01:18:55 +0200

    Added gallery validator

--- models/errors.go
+++ models/errors.go
@@ -6,9 +6,6 @@ const (
 	// ErrNotFound is returned when a resource cannot be found in the database
 	ErrNotFound modelError = "models: resource not found"
 
-	// ErrIDInvalid is returned when an invalid ID is provided
-	ErrIDInvalid modelError = "models: ID provided was invalid"
-
 	// ErrPasswordIncorrect is returned when an invalid password
 	// is used when attenpting to authenticate a user.
 	ErrPasswordIncorrect modelError = "models: incorrect password provided"
@@ -28,12 +25,21 @@ const (
 	// ErrPasswordRequired is returned when password is not set
 	ErrPasswordRequired modelError = "models: password is required"
 
+	// ErrTitleRequired when gallery title is not provided
+	ErrTitleRequired modelError = "models: title is required"
+
+	// ErrIDInvalid is returned when an invalid ID is provided
+	ErrIDInvalid privateError = "models: ID provided was invalid"
+
 	// ErrRememberTooShort is returned when the remember token is less than 32 bytes
-	ErrRememberTooShort modelError = "models: remeber token mus be at least 32 bytes"
+	ErrRememberTooShort privateError = "models: remeber token must be at least 32 bytes"
 
 	// ErrRememberRequired is returned when create or update is attempted
 	// without a user remeber token hash
-	ErrRememberRequired modelError = "models: remember token hash is required"
+	ErrRememberRequired privateError = "models: remember token hash is required"
+
+	// ErrUserIDRequired requires id
+	ErrUserIDRequired privateError = "models: user ID is required"
 )
 
 type modelError string
@@ -45,4 +51,10 @@ func (e modelError) Error() string {
 func (e modelError) Public() string {
 	s := strings.Replace(string(e), "models: ", "", 1)
 	return strings.Title(s)
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
