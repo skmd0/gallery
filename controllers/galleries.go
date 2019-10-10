78ac9bdef9c82ddc7b6a3634a9454ee7b9709fd1
Domen Skamlic <skamlic@posteo.net>
2019-10-10 22:15:15 +0200

    Added a delete action to the galleries controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -143,6 +143,31 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, url.Path, http.StatusFound)
 }
 
+// Delete deletes the gallery
+//
+// POST /galleries/:id/delete
+func (g *Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	err = g.gs.Delete(gallery.ID)
+	if err != nil {
+		vd.SetAlert(err)
+		vd.Yield = gallery
+		g.EditView.Render(w, vd)
+		return
+	}
+	// TODO: redirect to index page
+	fmt.Println("Sucessfully deleted!")
+}
+
 // GET /galleries/:id
 func (g *Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models.Gallery, error) {
 	vars := mux.Vars(r)
