29e9ece8cd4d3c36f70525ff341ac5e201c27946
Domen Skamlic <skamlic@posteo.net>
2019-10-10 22:47:33 +0200

    Added Gallery index page

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -14,29 +14,47 @@ import (
 
 const (
 	ShowGallery = "show_gallery"
+	EditGallery = "edit_gallery"
 )
 
 func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 	return &Galleries{
-		New:      views.NewView("bootstrap", "galleries/new"),
-		ShowView: views.NewView("bootstrap", "galleries/show"),
-		EditView: views.NewView("bootstrap", "galleries/edit"),
-		gs:       gs,
-		r:        r,
+		New:       views.NewView("bootstrap", "galleries/new"),
+		ShowView:  views.NewView("bootstrap", "galleries/show"),
+		EditView:  views.NewView("bootstrap", "galleries/edit"),
+		IndexView: views.NewView("bootstrap", "galleries/index"),
+		gs:        gs,
+		r:         r,
 	}
 }
 
 // Galleries is a wrapper over View struct used for rendering the template
 type Galleries struct {
-	New      *views.View
-	ShowView *views.View
-	EditView *views.View
-	gs       models.GalleryService
-	r        *mux.Router
+	New       *views.View
+	ShowView  *views.View
+	EditView  *views.View
+	IndexView *views.View
+	gs        models.GalleryService
+	r         *mux.Router
 }
 
 type GalleryForm struct {
 	Title string `schema:"title"`
+}
+
+// Index show all user's galleries
+//
+// GET /galleries
+func (g *Galleries) Index(w http.ResponseWriter, r *http.Request) {
+	user := context.User(r.Context())
+	galleries, err := g.gs.ByUserID(user.ID)
+	if err != nil {
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	var vd views.Data
+	vd.Yield = galleries
+	g.IndexView.Render(w, vd)
 }
 
 // GET /galleries/:id
@@ -134,7 +152,7 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 		g.New.Render(w, vd)
 		return
 	}
-	url, err := g.r.Get(ShowGallery).URL("id", fmt.Sprintf("%v", gallery.ID))
+	url, err := g.r.Get(EditGallery).URL("id", fmt.Sprintf("%v", gallery.ID))
 	if err != nil {
 		// TODO: make this go to the gallery index page
 		http.Redirect(w, r, "/", http.StatusFound)
@@ -164,8 +182,7 @@ func (g *Galleries) Delete(w http.ResponseWriter, r *http.Request) {
 		g.EditView.Render(w, vd)
 		return
 	}
-	// TODO: redirect to index page
-	fmt.Println("Sucessfully deleted!")
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 // GET /galleries/:id
--- models/galleries.go
+++ models/galleries.go
@@ -18,6 +18,7 @@ type GalleryService interface {
 
 type GalleryDB interface {
 	ByID(id uint) (*Gallery, error)
+	ByUserID(userID uint) ([]Gallery, error)
 	Create(gallery *Gallery) error
 	Update(gallery *Gallery) error
 	Delete(id uint) error
@@ -94,6 +95,13 @@ func (gg *galleryGorm) ByID(id uint) (*Gallery, error) {
 	db := gg.db.Where("id = ?", id)
 	err := first(db, &gallery)
 	return &gallery, err
+}
+
+// ByID will look up gallery by id and return the object
+func (gg *galleryGorm) ByUserID(id uint) ([]Gallery, error) {
+	var galleries []Gallery
+	gg.db.Where("user_id = ?", id).Find(&galleries)
+	return galleries, nil
 }
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
