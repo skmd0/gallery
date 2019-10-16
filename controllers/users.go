581454c3ca4bcd79e1c2566eb3dd402ad86fba2f
Domen Skamlic <skamlic@posteo.net>
2019-10-16 02:23:15 +0200

    Finished implementing redirect with alert

--- controllers/users.go
+++ controllers/users.go
@@ -67,7 +67,11 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
-	http.Redirect(w, r, "/galleries", http.StatusFound)
+	alert := views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Welcome to Gallery!",
+	}
+	views.RedirectAlert(w, r, "/galleries", http.StatusFound, alert)
 }
 
 // LoginForm contains fields for mapping login form data
